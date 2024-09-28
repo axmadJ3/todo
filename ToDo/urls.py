b2252bc0b1ca9881bd7be50994ead8dcc982a6f0
axmadJ3 <axmabdukarimov0304@outlook.com>
2024-09-28 22:54:03 +0500

    fix ...

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('add-todo/', views.addTodo, name='add-todo'),
     path('completed/', views.completed_todos, name='completed-todos'),
     path('completed/<int:pk>', views.execute_completed, name='completed'),
